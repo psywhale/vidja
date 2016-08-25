e4504949f74ee4cad7cda48f3c869a9f488dbdc5
psywhale <psycho.whale@gmail.com>
2016-08-25 13:47:31 -0500

    try again

--- config-dist.php
+++ config-dist.php
@@ -8,3 +8,9 @@ $CFG->dbsocket="";
 $CFG->dbuser="";
 $CFG->dbpassword="";
 $CFG->dbname="vidja";
+
+$CFG->workdir="/var/www/working";
+$CFG->videostore="/var/www/html/movietest";
+$CFG->thumbstore="/var/www/html/movietest";
+
+require_once("lib/load.php");
